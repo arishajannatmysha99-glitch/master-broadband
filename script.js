20af96c0b88965fcfd204f696e8485ec5da3151c
arishajannatmysha99-glitch <arishajannatmysha99@gmail.com>
2026-06-26 13:16:48 +0600

    Create script.js
    document.addEventListener("DOMContentLoaded", function () {
    
        console.log("Master Broadband Website Loaded");
    
        alert("🎉 Master Broadband Website-এ আপনাকে স্বাগতম!");
    
    });

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Master Broadband Website Loaded");
+
+    alert("🎉 Master Broadband Website-এ আপনাকে স্বাগতম!");
+
+});
