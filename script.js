8a0a692624ac501e9338d4b31c6cb68f3257eda8
arishajannatmysha99-glitch <arishajannatmysha99@gmail.com>
2026-06-26 18:31:44 +0600

    Update script.js
    function checkStatus() {
      const status = document.getElementById("status");
    
      if (navigator.onLine) {
        status.innerHTML = "🟢 Online - Network is Stable";
        status.style.color = "#00ff88";
      } else {
        status.innerHTML = "🔴 Offline - Connection Lost";
        status.style.color = "red";
      }
    }
    
    window.addEventListener("online", checkStatus);
    window.addEventListener("offline", checkStatus);

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-document.querySelector('.btn').addEventListener('click', (e) => {
-  e.preventDefault();
-  document.querySelector('#packages').scrollIntoView({ behavior: 'smooth' });
-});
+function checkStatus() {
+  const status = document.getElementById("status");
+
+  if (navigator.onLine) {
+    status.innerHTML = "🟢 Online - Network is Stable";
+    status.style.color = "#00ff88";
+  } else {
+    status.innerHTML = "🔴 Offline - Connection Lost";
+    status.style.color = "red";
+  }
+}
+
+window.addEventListener("online", checkStatus);
+window.addEventListener("offline", checkStatus);
