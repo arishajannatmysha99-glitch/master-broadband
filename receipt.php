14d38f23f99e34b74eda4b9a9f6182c335247bfd
arishajannatmysha99-glitch <arishajannatmysha99@gmail.com>
2026-06-27 17:59:12 +0600

    Create receipt.php
    <div class="container mt-4">
    
    <div class="card shadow">
    
    <div class="card-header bg-primary text-white text-center">
    <h3>MASTER BROADBAND</h3>
    <p class="mb-0">Internet Payment Receipt</p>
    </div>
    
    <div class="card-body">
    
    <table class="table table-bordered">
    
    <tr>
    <th width="220">Receipt No</th>
    <td>RC-0001</td>
    </tr>
    
    <tr>
    <th>Customer ID</th>
    <td>MB001</td>
    </tr>
    
    <tr>
    <th>Customer Name</th>
    <td>Md. Rahim</td>
    </tr>
    
    <tr>
    <th>Phone Number</th>
    <td>01982022884</td>
    </tr>
    
    <tr>
    <th>Package</th>
    <td>20 Mbps</td>
    </tr>
    
    <tr>
    <th>Billing Month</th>
    <td>June 2026</td>
    </tr>
    
    <tr>
    <th>Payment Amount</th>
    <td>800 ৳</td>
    </tr>
    
    <tr>
    <th>Payment Method</th>
    <td>Cash</td>
    </tr>
    
    <tr>
    <th>Transaction ID</th>
    <td>N/A</td>
    </tr>
    
    <tr>
    <th>Payment Date</th>
    <td>27-06-2026</td>
    </tr>
    
    </table>
    
    <div class="text-center mt-4">
    
    <button onclick="window.print()" class="btn btn-success">
    Print Receipt
    </button>
    
    <a href="payment_history.php" class="btn btn-secondary">
    Back
    </a>
    
    </div>
    
    </div>
    
    </div>
    
    </div>

---
+++ receipt.php
@@ -0,0 +1,82 @@
+<div class="container mt-4">
+
+<div class="card shadow">
+
+<div class="card-header bg-primary text-white text-center">
+<h3>MASTER BROADBAND</h3>
+<p class="mb-0">Internet Payment Receipt</p>
+</div>
+
+<div class="card-body">
+
+<table class="table table-bordered">
+
+<tr>
+<th width="220">Receipt No</th>
+<td>RC-0001</td>
+</tr>
+
+<tr>
+<th>Customer ID</th>
+<td>MB001</td>
+</tr>
+
+<tr>
+<th>Customer Name</th>
+<td>Md. Rahim</td>
+</tr>
+
+<tr>
+<th>Phone Number</th>
+<td>01982022884</td>
+</tr>
+
+<tr>
+<th>Package</th>
+<td>20 Mbps</td>
+</tr>
+
+<tr>
+<th>Billing Month</th>
+<td>June 2026</td>
+</tr>
+
+<tr>
+<th>Payment Amount</th>
+<td>800 ৳</td>
+</tr>
+
+<tr>
+<th>Payment Method</th>
+<td>Cash</td>
+</tr>
+
+<tr>
+<th>Transaction ID</th>
+<td>N/A</td>
+</tr>
+
+<tr>
+<th>Payment Date</th>
+<td>27-06-2026</td>
+</tr>
+
+</table>
+
+<div class="text-center mt-4">
+
+<button onclick="window.print()" class="btn btn-success">
+Print Receipt
+</button>
+
+<a href="payment_history.php" class="btn btn-secondary">
+Back
+</a>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
