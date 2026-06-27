a3f94e442a606d1f5319550e670fa8769a891456
arishajannatmysha99-glitch <arishajannatmysha99@gmail.com>
2026-06-27 17:32:55 +0600

    Create customer_profile.php
    <div class="container mt-4">
    
    <div class="card shadow">
    
    <div class="card-header bg-primary text-white">
    <h4>Customer Profile</h4>
    </div>
    
    <div class="card-body">
    
    <table class="table table-bordered">
    
    <tr>
    <th width="220">Customer ID</th>
    <td>MB001</td>
    </tr>
    
    <tr>
    <th>Full Name</th>
    <td>Md. Rahim</td>
    </tr>
    
    <tr>
    <th>Phone</th>
    <td>01982022884</td>
    </tr>
    
    <tr>
    <th>Address</th>
    <td>Bonmala Rail Crossing, Dattapara, Tongi, Gazipur</td>
    </tr>
    
    <tr>
    <th>Package</th>
    <td>20 Mbps</td>
    </tr>
    
    <tr>
    <th>Monthly Bill</th>
    <td>800 ৳</td>
    </tr>
    
    <tr>
    <th>Status</th>
    <td>
    <span class="badge bg-success">Active</span>
    </td>
    </tr>
    
    <tr>
    <th>Due</th>
    <td class="text-danger">
    400 ৳
    </td>
    </tr>
    
    <tr>
    <th>Join Date</th>
    <td>01-01-2026</td>
    </tr>
    
    <tr>
    <th>Last Payment</th>
    <td>05-06-2026</td>
    </tr>
    
    </table>
    
    <a href="customer_edit.php?id=1" class="btn btn-warning">
    Edit
    </a>
    
    <a href="customer_delete.php?id=1"
    class="btn btn-danger"
    onclick="return confirm('Delete Customer?')">
    Delete
    </a>
    
    <a href="customers.php" class="btn btn-secondary">
    Back
    </a>
    
    </div>
    </div>
    </div>

---
+++ customer_profile.php
@@ -0,0 +1,85 @@
+<div class="container mt-4">
+
+<div class="card shadow">
+
+<div class="card-header bg-primary text-white">
+<h4>Customer Profile</h4>
+</div>
+
+<div class="card-body">
+
+<table class="table table-bordered">
+
+<tr>
+<th width="220">Customer ID</th>
+<td>MB001</td>
+</tr>
+
+<tr>
+<th>Full Name</th>
+<td>Md. Rahim</td>
+</tr>
+
+<tr>
+<th>Phone</th>
+<td>01982022884</td>
+</tr>
+
+<tr>
+<th>Address</th>
+<td>Bonmala Rail Crossing, Dattapara, Tongi, Gazipur</td>
+</tr>
+
+<tr>
+<th>Package</th>
+<td>20 Mbps</td>
+</tr>
+
+<tr>
+<th>Monthly Bill</th>
+<td>800 ৳</td>
+</tr>
+
+<tr>
+<th>Status</th>
+<td>
+<span class="badge bg-success">Active</span>
+</td>
+</tr>
+
+<tr>
+<th>Due</th>
+<td class="text-danger">
+400 ৳
+</td>
+</tr>
+
+<tr>
+<th>Join Date</th>
+<td>01-01-2026</td>
+</tr>
+
+<tr>
+<th>Last Payment</th>
+<td>05-06-2026</td>
+</tr>
+
+</table>
+
+<a href="customer_edit.php?id=1" class="btn btn-warning">
+Edit
+</a>
+
+<a href="customer_delete.php?id=1"
+class="btn btn-danger"
+onclick="return confirm('Delete Customer?')">
+Delete
+</a>
+
+<a href="customers.php" class="btn btn-secondary">
+Back
+</a>
+
+</div>
+</div>
+</div>
